36862d8affb339b56f5c927c3d43d36d241b83f2
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2021-06-07 15:35:05 +0530

    Create 1389-Create Target Array in the Given Order.py

---
+++ 1389-Create Target Array in the Given Order.py
@@ -0,0 +1,7 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        lst=[]
+        for i in range(len(nums)):
+            lst.insert(index[i],nums[i])
+        return lst
+        
