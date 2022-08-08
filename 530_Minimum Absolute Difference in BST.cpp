baf32d8392497dab545edad9c6bf4da5af4d4b32
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2022-08-08 15:24:32 +0530

    Create 530_Minimum Absolute Difference in BST.cpp

---
+++ 530_Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    TreeNode *prev=NULL;
+    int diff=INT_MAX;
+    void dfs(TreeNode *root)
+    {
+        if(root->left) dfs(root->left);
+        if(prev) diff=min(diff,abs(prev->val-root->val));
+        prev=root;
+        if(root->right) dfs(root->right);
+    }
+    int getMinimumDifference(TreeNode* root) 
+    {
+        dfs(root);
+        return diff;
+    }
+};
