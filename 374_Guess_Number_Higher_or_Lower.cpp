e2f65db2d7808d184a93fba230024917ecbf01e5
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2022-11-16 16:02:55 +0530

    Create 374_Guess_Number_Higher_or_Lower.cpp

---
+++ 374_Guess_Number_Higher_or_Lower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int guessNumber(int n) 
+    {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+            if (res == 0)
+                return mid;
+            else if (res < 0)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+};
